e8b72e7fea0fdc358958d9f05c2e99366d9f5ebc
Opera1122 <113095585+Opera1122@users.noreply.github.com>
2022-09-21 10:13:55 +0900

    Add files via upload

---
+++ DrinkOrderProgram/DrinkOrderProgram/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace DrinkOrderProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(23, 113);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(205, 172);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "2000원";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "2200원";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "2500원";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(386, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "1500원";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "딸기주스";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "망고주스";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(258, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "초코라떼";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(369, 14);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "아메리카노";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(21, 292);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 32);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "삭제";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(256, 156);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(188, 32);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "10000원 투입";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(21, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(207, 21);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "장바구니";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(256, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(188, 24);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "잔액";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Location = new System.Drawing.Point(256, 132);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(188, 21);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "0원";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(256, 209);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(188, 23);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "결제금액";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label9.Location = new System.Drawing.Point(256, 235);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(188, 21);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "0원";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(256, 259);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(188, 32);
+            this.button7.TabIndex = 21;
+            this.button7.Text = "결제 및 영수증 출력";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(125, 292);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(103, 32);
+            this.button8.TabIndex = 22;
+            this.button8.Text = "전체 삭제";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 336);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "음료수 주문 프로그램";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ DrinkOrderProgram/DrinkOrderProgram/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DrinkOrderProgram
+{
+    public partial class Form1 : Form
+    {
+        int leftMoney = 0; //잔금
+        int payout = 0; //결제금액
+        int order = 0; //주문
+        object text = null;
+        string receipt = "영수증.txt";
+        int drinkPrice = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("딸기주스");
+            payout += 2000;
+            label9.Text = Convert.ToString(payout) + "원";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("망고주스");
+            payout += 2200;
+            label9.Text = Convert.ToString(payout) + "원";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("초코라떼");
+            payout += 2500;
+            label9.Text = Convert.ToString(payout) + "원";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("아메리카노");
+            payout += 1500;
+            label9.Text = Convert.ToString(payout) + "원";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > -1)
+            {
+                listBox1.SelectedItem.ToString();
+
+                if (listBox1.SelectedItem.Equals("딸기주스"))
+                {
+                    payout -= 2000;
+                    label9.Text = Convert.ToString(payout) + "원";
+                }
+                else if (listBox1.SelectedItem.Equals("망고주스"))
+                {
+                    payout -= 2200;
+                    label9.Text = Convert.ToString(payout) + "원";
+                }
+                else if (listBox1.SelectedItem.Equals("초코라떼"))
+                {
+                    payout -= 2500;
+                    label9.Text = Convert.ToString(payout) + "원";
+                }
+                else if (listBox1.SelectedItem.Equals("아메리카노"))
+                {
+                    payout -= 1500;
+                    label9.Text = Convert.ToString(payout) + "원";
+                }
+
+                Convert.ToString(listBox1.SelectedIndex);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            leftMoney += 10000;
+            label7.Text = Convert.ToString(leftMoney) + "원";
+        }
+
+        private void CreateFile()
+        {
+            int nCount = listBox1.Items.Count;
+            for (int i = 0; i < nCount; i++)
+            {
+                if (listBox1.Items[i].Equals("딸기주스"))
+                {
+                    drinkPrice = 2000;
+                }
+                else if (listBox1.Items[i].Equals("망고주스"))
+                {
+                    drinkPrice = 2200;
+                }
+                else if (listBox1.Items[i].Equals("초코라떼"))
+                {
+                    drinkPrice = 2500;
+                }
+                else if (listBox1.Items[i].Equals("아메리카노"))
+                {
+                    drinkPrice = 1500;
+                }
+                text = listBox1.Items[i] + " " + drinkPrice + "원" + "\n";
+                File.AppendAllText(receipt, (string)text);
+            }
+            text = "\n총 결제금액 " + payout + "원" + "\n" + "잔액 " + leftMoney + "원" + "\n\n\n";
+            File.AppendAllText(receipt, (string)text);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            order = 0;
+            if (leftMoney == 0 || leftMoney < payout)
+            {
+                MessageBox.Show("돈이 부족합니다.");
+            }
+            else if (payout != 0 && leftMoney >= payout)
+            {
+                order = 1;
+                leftMoney -= payout;
+                label7.Text = Convert.ToString(leftMoney) + "원";
+                label9.Text = Convert.ToString(0) + "원";
+                CreateFile();
+                payout = 0;
+                listBox1.Items.Clear();
+                MessageBox.Show("결제가 완료되었습니다. 영수증을 확인하세요.");
+            }
+            else
+            {
+                MessageBox.Show("결제할 상품이 없습니다.");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            payout = 0;
+            label9.Text = Convert.ToString(payout) + "원";
+        }
+    }
+}
